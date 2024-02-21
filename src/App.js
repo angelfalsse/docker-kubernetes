f859ce3d915fe522f312140318930d521c837901
angelfalsse <takeru.imada@gmail.com>
2024-02-22 02:09:06 +0900

    feat: changed log

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey there!
+          We changed here!
         </p>
         <a
           className="App-link"
